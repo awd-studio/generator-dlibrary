2585dc71b158fcf9c5feda7300aba80433718e38
Anton Karpov <awd.com.ua@gmail.com>
2017-10-26 08:45:40 +0300

    Init

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-dlibrary [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Drupal Library integrate Generator
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-dlibrary using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-dlibrary
+```
+
+Then generate your new project:
+
+```bash
+yo dlibrary
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Anton Karpov](https://github.com/awd-studio)
+
+
+[npm-image]: https://badge.fury.io/js/generator-dlibrary.svg
+[npm-url]: https://npmjs.org/package/generator-dlibrary
+[travis-image]: https://travis-ci.org/awd-studio/generator-dlibrary.svg?branch=master
+[travis-url]: https://travis-ci.org/awd-studio/generator-dlibrary
+[daviddm-image]: https://david-dm.org/awd-studio/generator-dlibrary.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/awd-studio/generator-dlibrary
+[coveralls-image]: https://coveralls.io/repos/awd-studio/generator-dlibrary/badge.svg
+[coveralls-url]: https://coveralls.io/r/awd-studio/generator-dlibrary
---
+++ generators/app/index.js
@@ -0,0 +1,270 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const path = require('path');
+
+// Generator constants
+const drupal7 = '7';
+const drupal8 = '8';
+
+module.exports = class extends Generator {
+  prompting() {
+    // Base dir path
+    this.baseDir = path.basename(process.cwd());
+
+    // Module machine name
+    this.moduleNameMachine = this.baseDir.toLowerCase().replace(/[^a-z0-9]/gi, '_');
+    this.moduleNameDefault =
+      this.moduleNameMachine.charAt(0).toUpperCase() + this.moduleNameMachine.slice(1);
+    this.moduleDescriptionDefault = 'My custom module';
+
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(
+        'Yo! Here is an Awesome ' + chalk.red('Drupal Library Module') + ' generator!'
+      )
+    );
+
+    /** Promting */
+    const prompts = [
+      {
+        type: 'list',
+        name: 'drupalCore',
+        message: 'Choose Drupal core:',
+        default: drupal7,
+        choices: [drupal7, drupal8]
+      },
+      {
+        name: 'moduleNameHuman',
+        message: 'Say me the name of your module:',
+        default: this.moduleNameDefault.replace(/_/gi, ' ')
+      },
+      {
+        name: 'moduleDescription',
+        message: 'Describe your module:',
+        default: this.moduleDescriptionDefault
+      },
+      {
+        name: 'modulePackage',
+        message: 'Set the package:',
+        default: 'Custom'
+      },
+      {
+        name: 'moduleDependencies',
+        message: 'Module dependencies ' + chalk.dim('(space or comma separated)') + ':'
+      },
+      {
+        type: 'list',
+        name: 'libraryType',
+        message: 'Yype of library:',
+        default: 'js',
+        choices: ['js', 'php']
+      },
+      {
+        name: 'libraryFile',
+        message: 'Library File (with path):',
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return chalk.bgRed('This option is required!');
+          }
+
+          return true;
+        },
+        default: 'vue/vue.min.js'
+      },
+      {
+        name: 'libraryName',
+        message: 'Library Name:',
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return chalk.bgRed('This option is required!');
+          }
+
+          return true;
+        },
+        default: 'Vue.js' // ToDo: Delete!
+      },
+      {
+        name: 'libraryUrl',
+        message: 'Library homepage URL:',
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return chalk.bgRed('This option is required!');
+          }
+
+          return true;
+        },
+        default: 'https://vue_js.org/' // ToDo: Delete!
+      },
+      {
+        name: 'libraryUrlDownload',
+        message: 'Library archive Download URL:',
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return chalk.bgRed('This option is required!');
+          }
+
+          return true;
+        },
+        default: 'https://github.com/vuejs/vue/archive/master.zip' // ToDo: Delete!
+      },
+      {
+        name: 'libraryVersionFile',
+        message: 'Library Version Arguments File:',
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return chalk.bgRed('This option is required!');
+          }
+
+          return true;
+        },
+        default: 'vue/vue.min.js' // ToDo: Delete!
+      },
+      {
+        name: 'libraryVersionPattern',
+        message: 'Library Version Arguments Pattern:',
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return chalk.bgRed('This option is required!');
+          }
+
+          return true;
+        },
+        default: 'v(\\d+\\.\\d+\\.\\d+)' // ToDo: Delete!
+      },
+      {
+        name: 'libraryVersionLines',
+        message: 'Library Version Arguments Lines:',
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return chalk.bgRed('This option is required!');
+          }
+
+          if (typeof answer !== 'number') {
+            return chalk.bgRed('You need to provide a number!');
+          }
+
+          return true;
+        },
+        default: 2 // ToDo: Delete!
+      },
+      {
+        name: 'libraryExtractPreffix',
+        message: 'Library extraction preffix:',
+        default: 'vue-' // ToDo: Delete!
+      },
+      {
+        type: 'confirm',
+        name: 'drushCommand',
+        message: 'Implement Drush Command?'
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    /**
+     * Variables
+     */
+    this.data = {
+      drupalCore: this.props.drupalCore,
+      libraryType: this.props.libraryType,
+      libraryFile: this.props.libraryFile,
+      libraryName: this.props.libraryName,
+      libraryUrl: this.props.libraryUrl,
+      libraryUrlDownload: this.props.libraryUrlDownload,
+      libraryVersionFile: this.props.libraryVersionFile,
+      libraryVersionPattern: this.props.libraryVersionPattern,
+      libraryVersionLines: this.props.libraryVersionLines,
+      libraryExtractPreffix: this.props.libraryExtractPreffix,
+      moduleNameMachine: this.moduleNameMachine,
+      moduleNameDash: this.moduleNameMachine.replace(/_/, '-'),
+      moduleNameHuman: this.props.moduleNameHuman,
+      moduleNameUpper: this.moduleNameMachine.toUpperCase(),
+      moduleDescription: this.props.moduleDescription,
+      modulePackage: this.props.modulePackage,
+      moduleDependencies:
+        'dependencies[] = ' +
+        ('libraries' +
+          (this.props.moduleDependencies.length ? ' ' : '') +
+          this.props.moduleDependencies
+        )
+          .split(' ')
+          .join('\ndependencies[] = ')
+    };
+
+    if (this.props.drupalCore === drupal7) {
+      /**
+       * Create module.info
+       */
+      this.fs.copyTpl(
+        this.templatePath(this.props.drupalCore + '/_template.info'),
+        this.destinationPath(this.moduleNameMachine + '.info'),
+        this.data
+      );
+
+      /**
+       * Create module.module
+       */
+      this.fs.copyTpl(
+        this.templatePath(this.props.drupalCore + '/_template.module'),
+        this.destinationPath(this.moduleNameMachine + '.module'),
+        this.data
+      );
+
+      /**
+       * Create module.install
+       */
+      this.fs.copyTpl(
+        this.templatePath(this.props.drupalCore + '/_template.install'),
+        this.destinationPath(this.moduleNameMachine + '.install'),
+        this.data
+      );
+
+      /**
+       * Create Drush command
+       */
+      if (this.props.drushCommand) {
+        this.fs.copyTpl(
+          this.templatePath(this.props.drupalCore + '/drush/_template.drush.inc'),
+          this.destinationPath('./drush/' + this.moduleNameMachine + '.drush.inc'),
+          this.data
+        );
+      }
+    } else if (this.props.drupalCore === drupal8) {
+      /**
+       * Create module.info
+       */
+      this.fs.copyTpl(
+        this.templatePath(this.props.drupalCore + '/_template.info.yml'),
+        this.destinationPath(this.moduleNameMachine + '.info.yml'),
+        this.data
+      );
+
+      /**
+       * Create module.module
+       */
+      this.fs.copyTpl(
+        this.templatePath(this.props.drupalCore + '/_template.module'),
+        this.destinationPath(this.moduleNameMachine + '.module'),
+        this.data
+      );
+
+      /**
+       * Create module.install
+       */
+      this.fs.copyTpl(
+        this.templatePath(this.props.drupalCore + '/_template.install'),
+        this.destinationPath(this.moduleNameMachine + '.install'),
+        this.data
+      );
+    } else {
+      console.error('Drupal Core not allowed!');
+    }
+  }
+};
